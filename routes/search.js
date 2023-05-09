f18da0dad542dc822a5104cf28b60f85df9f5bf9
Nguyen Xuan Hieu <hieu.nguyenxuan@decision-science.com>
2023-05-10 02:58:13 +0700

    feat: add search suggestions api

--- routes/search.js
+++ routes/search.js
@@ -5,24 +5,30 @@ router.get('/', (req, res) => {
   const currentLoadMore = +req.query.loadmore;
   const loadMore = currentLoadMore >= 4 ? false : true;
   const data = {
-    "total": 836,
-    "currentLoadMore": currentLoadMore+1,
-    "limit": 20,
-    "maxNumberLoadMore": 42,
-    "loadMore": loadMore,
-    "results": [
+    total: 836,
+    currentLoadMore: currentLoadMore + 1,
+    limit: 20,
+    maxNumberLoadMore: 42,
+    loadMore: loadMore,
+    results: [
       {
-        "date": "21-Apr-2023",
-        "path": "/content/public/sg/en/public/test-center.html",
-        "size": "",
-        "title": "Test center",
-        "contentType": "page",
-        "desc": "<p class='search-result-item__description CoveoExcerpt'>Protect you and your family against the unexpected with our range of plans to suit your needs. Test Test Test Test Test Test Test Test 1 Test Test Test Test Test Test Test Test 2 Test Test Test Test T...</p>",
-        "dataType": "all"
+        date: '21-Apr-2023',
+        path: '/content/public/sg/en/public/test-center.html',
+        size: '',
+        title: 'Test center',
+        contentType: 'page',
+        desc: "<p class='search-result-item__description CoveoExcerpt'>Protect you and your family against the unexpected with our range of plans to suit your needs. Test Test Test Test Test Test Test Test 1 Test Test Test Test Test Test Test Test 2 Test Test Test Test T...</p>",
+        dataType: 'all',
       },
-    ]
-  }
-  res.json(data);
-})
+    ],
+  };
+  setTimeout(() => {
+    res.json(data);
+  }, 1000);
+});
+
+router.get('/suggestions', (req, res) => {
+  return res.json({ code: 200, msg: '', results: ['insurance', 'b'] });
+});
 
 module.exports = router;
